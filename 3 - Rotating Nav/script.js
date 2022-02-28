1bb037856427691c312cbcb3fd1454b1e9391381
mikemacfarland <m_macfarland@live.com>
2022-02-28 01:34:22 -0500

    project 3 - Rotating Nav

---
+++ 3 - Rotating Nav/script.js
@@ -0,0 +1,28 @@
+const openNav = document.getElementById('open')
+const closeNav = document.getElementById('close')
+
+const container = document.querySelector('.container')
+
+container.addEventListener('click', (e) =>{
+    if(e.target.id === "open"){
+        container.classList.add('show-nav')
+    }
+    if(e.target.id === "close"){
+        container.classList.remove('show-nav')
+    }
+})
+
+// ======== both of these work but the icon is not clickable within the button tags=======
+
+// const container = document.querySelector('.container')
+// const navButtons = document.getElementsByTagName('button')
+
+// navButtons.forEach(button => {
+//     button.addEventListener('click', (button) => {
+//         if (button.target.id === 'open'){
+//         Container.classList.add('show-nav')
+//         }
+//         if (button.target.id === 'close'){
+//         Container.classList.remove('show-nav')
+//         }
+// })})
