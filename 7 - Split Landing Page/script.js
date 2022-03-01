cb4d5f7f5d71ac099fc18c445b169d3c29ea8389
mikemacfarland <m_macfarland@live.com>
2022-03-01 13:21:23 -0500

    finished project 7 - Split Landing Page

--- 7 - Split Landing Page/script.js
+++ 7 - Split Landing Page/script.js
@@ -1,27 +1,19 @@
-const xbox = document.querySelector('.xbox')
-const ps5 = document.querySelector('.ps5')
 
-const containers = document.querySelectorAll('container')
+// using a mouseover event listener
 
-window.addEventListener('mouseover', (e) =>{
+// const xbox = document.querySelector('.xbox')
+// const ps5 = document.querySelector('.ps5')
+
+// const containers = document.querySelectorAll('container')
+
+// window.addEventListener('mouseover', (e) =>{
     
-    if (e.target = ps5){
-        ps5.classList.add('active')
-        xbox.classList.remove('active')
-    }
-    // else{
-    //     ps5.classList.remove('active')
-    // }
-    if (e.target === xbox){
-        xbox.classList.add('active')
-        ps5.classList.remove('active')
-    }
-    // else{
-    //     xbox.classList.remove('active')
-    // }
-})
-
-
-// if mouse is over container elements expand them
-
-// if mouse is not over anything remove active from everything (else)
+//     if (e.target === ps5){
+//         ps5.classList.add('active')
+//         xbox.classList.remove('active')
+//     }
+//     if (e.target === xbox){
+//         xbox.classList.add('active')
+//         ps5.classList.remove('active')
+//     }
+// })
