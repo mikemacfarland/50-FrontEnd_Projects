070e9626c99d69ea441d6a9ee1f788a8ebe06009
mikemacfarland <m_macfarland@live.com>
2022-03-14 14:45:10 -0400

    finished project 28 - Github Profiles Search

--- 28 -  Github Profiles/script.js
+++ 28 -  Github Profiles/script.js
@@ -3,43 +3,69 @@ const formEl = document.getElementById('form')
 const search = document.getElementById('search')
 const main = document.getElementById('main')
 
-
 formEl.addEventListener('submit', (e) =>{
     e.preventDefault()
-    getUser(search.value)
-    search.value = ''
+    createUserCard(search.value)
 })
 
-async function getUser(userName){
+async function createUserCard(userName){
     axios(APIURL + userName)
     // USING async await, USING try & catch (try code, catch error)
     try {
         const {data} = await axios(APIURL + userName)
-        console.log(data)
-
         const userCard = `<div class="card">
-            <div ><img class="avatar" src="${data.avatar_url}" alt="${data.name}"></div>
+            <div><img class="avatar" src="${data.avatar_url}" alt="${data.name}"></div>
             <div class="user-info">
                 <h2>${data.name}</h2>
                 <p>${data.bio}</p>
                 <ul>
-                    <li>${data.followers} <strong>Followers</strong></li>
+                    <li>${data.followers}<strong>Followers</strong></li>
                     <li>${data.following}<strong>Following</strong></li>
                     <li>${data.public_repos}<strong>Repositories</strong></li>
                 </ul>
-                <div id="repos">
-                    <h3>This user has no public repos</h3>
-                    <a href="#" class="repo">Repo 1</a>
-                    <a href="#" class="repo">Repo 1</a>
-                    <a href="#" class="repo">Repo 1</a>
-                </div>
+                <div id="repos"></div>
             </div>
         </div>`
-    main.innerHTML = userCard
-    }catch(err){
-        console.log(err)
+        main.innerHTML = userCard
+        getUserRepos(userName)
     }
-    // USING .then .catch, getting response. logging response, getting error if any & logging error
-    // .then(res => console.log(res))
-    // .catch(err => console.log(err))
+    catch (err) {
+        error = err.response
+        if (error.status === 404){
+            createErrorCard(`User "${search.value}" not found`,main)
+        }
+    }
+    search.value = ''
+}
+
+async function getUserRepos(userName){
+    axios(APIURL + userName + '/repos?sort=created')
+    const reposEl = document.getElementById('repos')
+        try{
+            const {data} = await axios(APIURL + userName + '/repos?sort=created')
+            repos = data
+            if(repos.length === 0){
+                createErrorCard('This user has no repos',reposEl)
+            }
+            else{
+            repos.splice(0,6).forEach((repo) =>{
+                repoEl = document.createElement('a')
+                repoEl.classList.add('repo')
+                repoEl.href = repo.html_url
+                repoEl.target = '_blank'
+                repoEl.innerText = repo.name
+                reposEl.append(repoEl)
+                })
+            }
+        }
+        catch (err){
+            console.log(error)
+            if (error.status === 404)
+            createErrorCard('This user has no repos',repos)
+        }
+}
+
+function createErrorCard(msg,targetEl){
+    const cardHTML = `<div class="error card"><h2>${msg}</h2></div>`
+    targetEl.innerHTML = cardHTML
 }
